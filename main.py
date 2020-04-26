ed9e18e1a08ebed99f5c268ffc69da5a38bd1274
Jandy4 <justandygames@gmail.com>
2020-04-27 00:33:22 +0300

    Bot working

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import os
+import random
 
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+import discord
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+STREAM = r'http://jazz-wr04.ice.infomaniak.ch/jazz-wr04-128.mp3'
 
-client = discord.Client()
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+bot = commands.Bot(command_prefix='!')
 
-client.run(TOKEN)
+@bot.command()
+async def play(ctx):
+    channel = ctx.author.voice.channel
+    vc = await channel.connect()
+    source = await discord.FFmpegOpusAudio.from_probe(STREAM, method='fallback')
+    vc.play(source)
+    
+
+@bot.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+bot.run(TOKEN)
