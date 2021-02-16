15a27309e89c1358dcad1479c78fa0d0fb2bd2b2
nan-dre <andu_niculae@yahoo.com>
2021-02-16 13:51:52 +0200

    cleaning

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import random
 
 from discord.ext import commands
-from dotenv import load_dotenv
 import discord
+from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
